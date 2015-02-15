55857946a7d2bc1ceb34620820751b6b3e94fe88
Mark Phillips <embeepea@git>
2015-02-15 02:44:09 +0000

    setup.sh is quiet now, and does not care about cloudconf

--- setup.sh
+++ setup.sh
@@ -8,18 +8,11 @@ if [ ! -d $DIR/cloudmanager ] ; then
     echo "cloudmanager subdirectory not found"
     exit -1
 fi
-if [ ! -d $DIR/cloudconf ] ; then
-    echo "cloudconf subdirectory not found"
-    exit -1
-fi
 
 
 function add_bindir_to_path() {
   if ! ( echo $PATH | grep "$1" > /dev/null ) ; then
     export PATH=$1:$PATH
-    echo "$1 added to PATH"
-  else
-    echo "$1 is already on PATH"
   fi
 }
 
